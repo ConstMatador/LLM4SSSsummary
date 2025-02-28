c5b10e3f8be90ffa943cf785eef547d03ecd277e
WangSl <wangsl@stu.hit.edu.cn>
2025-02-28 22:13:08 +0800

    111

---
+++ figure/draw.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+
+selected_model = "UniTime"
+
+# File Path
+file_path = f"figure/error/{selected_model}.txt"
+save_path = f"figure/figure/{selected_model}.png"
+
+data = []
+try:
+    with open(file_path, "r") as file:
+        for line in file:
+            try:
+                value = float(line.strip())
+                data.append(value)
+            except ValueError:
+                print(f"{line.strip()} can not be transformed to float")
+except FileNotFoundError:
+    print(f"{file_path} not found")
+
+# 确保读取到的浮点数数量为100
+if len(data) != 100:
+    print(f"Error: The number of data points is not 100, it is{len(data)}")
+
+# 绘制折线图
+plt.plot(data)
+plt.title(selected_model)
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.grid(True)
+plt.xticks(range(1, 101,5))
+
+plt.savefig(save_path)
---
+++ figure/extract.py
@@ -0,0 +1,20 @@
+import os
+
+selected_model = "UniTime"
+
+log_path = f"figure/log/{selected_model}.log"
+save_path = f"figure/error/{selected_model}.txt"
+
+first_extracted = False
+
+with open(log_path, "r") as log_file:
+    with open(save_path, "w") as output_file:
+        for line in log_file:
+            if "validate trans_err: " in line:
+                trans_err_value = line.split("validate trans_err: ")[1].strip()
+                
+                if not first_extracted:
+                        first_extracted = True
+                        continue
+                
+                output_file.write(trans_err_value + "\n")

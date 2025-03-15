39d3e13731d27293cd8b889560ff885c934c8061
WangSl <wangsl@stu.hit.edu.cn>
2025-03-15 23:51:11 +0800

    111

--- 1stBSF_Tightness/nearest.py
+++ 1stBSF_Tightness/nearest.py
@@ -1,20 +1,26 @@
 import numpy as np
+from scipy.spatial import cKDTree
+from tqdm import tqdm
 
 
 model_selected = "S2IPLLM"
 
-data_path = "1stBSF_Data/" + model_selected + "/reduce_data.bin"
-query_path = "1stBSF_Data/" + model_selected + "/reduce_query.bin"
-output_path = "1stBSF_Tightness/" + model_selected + "/exact.txt"
+data_path = f"1stBSF_Data/{model_selected}/origin_data.bin"
+query_path = f"1stBSF_Data/{model_selected}/origin_query.bin"
+output_path = f"1stBSF_Tightness/{model_selected}/exact.txt"
 
 data = np.fromfile(data_path, dtype=np.float32).reshape(1000000, 256)
 queries = np.fromfile(query_path, dtype=np.float32).reshape(1000, 256)
 
+tree = cKDTree(data)
+
 nearest_indices = []
-for i, q in enumerate(queries):
-    distances = np.linalg.norm(data - q, axis=1)  
-    nearest_idx = np.argmin(distances)  
-    nearest_indices.append(nearest_idx)
+batch_size = 100
+
+for i in tqdm(range(0, len(queries), batch_size), desc="Processing Queries", unit="batch"):
+    batch_queries = queries[i:i+batch_size]
+    _, batch_nearest_indices = tree.query(batch_queries, k=1)
+    nearest_indices.extend(batch_nearest_indices)
 
 with open(output_path, "w") as f:
     f.write("\n".join(map(str, nearest_indices)))
